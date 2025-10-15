dc04011feb6602dbf8427011970f602bd936f6ab
innocent <innocentkamesa05@gmail.com>
2025-10-15 17:26:26 +0200

    Setup task management request methods, next up setting up urls and testing

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from .serializers import TaskSerializer, UserSerializer
 from rest_framework.response import Response 
 from rest_framework import permissions
 from user.models import User
+from task.models import Task
 
 class TaskView(APIView):
     def post(self, request, *args, **kwargs):
@@ -16,6 +17,69 @@ class TaskView(APIView):
             return Response({"Success":serializer.data}, status=200)
         return Response({"Failure":serializer.errors}, status=400)
 
+    def get(self, request, *args, **kwargs):
+        if kwargs.get("id"):
+            instance = Task.objects.get(id=kwargs.get("id"), owner=request.user)
+            if instance:
+                serializer = TaskSerializer(instance=instance)
+                if serializer.is_valid():
+                    data = serializer.data
+                    return Response({"data":data}, status=200)
+                return Response({"error":serializer.errors}, status=402)
+            else:
+                return Response({"error":"Item not Found"}, status=404)
+        else:
+            tasks = Task.objects.filter(owner=request.user)
+            serializer = TaskSerializer(instance=tasks, many=True)
+            if tasks:
+                if serializer.is_valid():
+                    data = serializer.data
+                    return Response({"data":data}, status=200)
+                return Response({"error":serializer.errors}, status=402)
+             else:
+                return Response({"error":"Items not Found"}, status=404)
+
+
+    def patch(self, request, *args, **kwargs):
+        data = request.data
+        id = kwargs.get("id")
+        owner = request.user
+        instance = Task.objects.get(id=id, owner=owner)
+        if instance:
+            serializer = TaskSerializer(instance=instance, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                data = serializer.data
+                return Response({"Success":data}, status=200)
+            return Response({"error":serializer.errors}, status=402)
+        else:
+            return Response({"error":"Item not Found"}, status=404)
+
+
+    def put(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+        id = kwargs.get("id")
+        instance = Task.objects.get(owner=user, id =id)
+        if instance:
+            serializer = TaskSerializer(instance=instance, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                data = serializer.data
+                return Response({"Success":data}, status=200)
+            return Response({"error":serializer.errors}, status=402)
+        else:
+            return Response({"error":"Item not Found"}, status=404)
+    
+    def delete(self, request, *args, **kwargs):
+        id = kwargs.get('id')
+        owner = self.request.user
+        instance = Task.objects.get(id=id, owner=owner)
+        if instance:
+            instance.delete()
+            return Response({"Success":"Successfully deleted"}, status=204)
+        else:
+            return Response({"error":"Object not found"}, status=404)
 
 class UserView(APIView):
     def get_permissions(self):
@@ -32,7 +96,7 @@ class UserView(APIView):
     
     def patch(self, request, *args, **kwargs):
         user = self.request.user
-        serializer = UserSerializer(user, data=request.data, partial=True)
+        serializer = UserSerializer(instance=user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response({"Success":serializer.data}, status=200)
